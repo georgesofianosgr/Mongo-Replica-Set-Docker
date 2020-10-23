38e400140420e82db23a26f9e988db1f7be60fda
George Sofianos <george.sofianos@100mentors.com>
2020-10-23 21:59:56 +0300

    Un-comment replica config

--- rs-init.js
+++ rs-init.js
@@ -2,8 +2,8 @@ config = {
   _id: "rs1",
   members: [
     { _id: 0, host: "mongo-node-1:27017", priority: 2 },
-    // { _id: 1, host: "mongo-node-2:27018", priority: 1 },
-    // { _id: 2, host: "mongo-node-3:27019", priority: 1 }
+    { _id: 1, host: "mongo-node-2:27018", priority: 1 },
+    { _id: 2, host: "mongo-node-3:27019", priority: 1 }
   ]
 };
 rs.initiate(config);
